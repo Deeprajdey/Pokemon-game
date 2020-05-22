e89d7175c4cfc833b17be69238d2670b1b85418d
Deepraj Dey <ddeepraj47@gmail.com>
2020-05-22 17:38:40 +0530

    all edited

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 //import logo from "./logo.svg";
 import "./App.css";
+import Pokegame from "./Pokegame";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <Pokegame />
+      </header>
     </div>
   );
 }
